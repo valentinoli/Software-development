ec8c9d9d0a73387b7a8c0d732e76cf72cd598d7a
valentinoli <valentin081194@gmail.com>
2017-03-02 12:21:20 +0000

    Added further variables and getters, modified constructor

--- Hotel.java
+++ Hotel.java
@@ -2,13 +2,31 @@
 public class Hotel {
 	
 	private String name;
+	private String address;
+	private String phone;
+	private int price;
 	
-	public Hotel(String name) {
+	public Hotel(String name, String address, String phone, int price) {
 		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		this.price = price;
 	}
 	
 	public String getName() {
 		return name;
 	}
 
+	public String getAddress() {
+		return address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	
 }
