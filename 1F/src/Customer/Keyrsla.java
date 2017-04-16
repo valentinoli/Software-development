2aa4b3e1547566911330abe72572bc53a1428c37
steinnarnar <steinnarnar@gmail.com>
2017-04-16 09:33:06 +0000

    Uppfærsla á keyrsluskrá

--- 1F/src/Customer/Keyrsla.java
+++ 1F/src/Customer/Keyrsla.java
@@ -4,15 +4,27 @@ import java.util.Scanner;
 public class Keyrsla {
 	
 	public static void main(String [] args){
+		System.out.println("Insert 1 to search by location or 2 to search by date");
+		
 		Keyrsla a = new Keyrsla();
-		a.searchFlight();
+		a.searchFlightLocation();
+		//Keyrsla b = new Keyrsla();
+		//b.searchFlightDate();
 	}
-	private void searchFlight(){
+	private void searchFlightLocation(){
 		FlightSearch s = new FlightSearch();
 		System.out.println("Insert Location");
 		Scanner a = new Scanner(System.in);
 		String location = a.nextLine();
 		System.out.println(s.searchByDepartureLocation(location));
-	
 	}
+	/*private void searchFlightDate(){
+		FlightSearch t = new FlightSearch();
+		System.out.println("Insert Date");
+		Scanner b = new Scanner(System.in);
+		String date = b.nextLine();
+		System.out.println(t.searchByDate(date));
+	}
+	*/
+	
 }
